2a122c2adaf304fc7d09d99f33ca95e993387fbb
Anup Wade <anup.wade@outlook.com>
2026-07-25 13:04:06 +0530

    Fix CI/CD pipeline and Kubernetes health checks

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', getRoutes, name="routes"),
-    path('notes/', getNotes, name="notes"),
-    path('notes/<str:pk>/update/', updateNote, name="update-note"),
-    path('notes/<str:pk>/delete/', deleteNote, name="delete-note"),
-    path('notes/create/', createNote, name="create-note"),
-    path('notes/<str:pk>/', getNote, name="note"),
+    path("", getRoutes, name="routes"),
+    path("health/", health, name="health"),
+
+    path("notes/", getNotes, name="notes"),
+    path("notes/create/", createNote, name="create-note"),
+    path("notes/<str:pk>/", getNote, name="note"),
+    path("notes/<str:pk>/update/", updateNote, name="update-note"),
+    path("notes/<str:pk>/delete/", deleteNote, name="delete-note"),
 ]
--- api/views.py
+++ api/views.py
@@ -3,8 +3,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import NoteSerializer
 from .models import Note
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def health(request):
+    return JsonResponse({"status": "ok"})
 
 @api_view(['GET'])
 def getRoutes(request):
